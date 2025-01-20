49a7a724a63a89690ab8bc1074b962bc2f159b5a
NachoHerrero <116599852+NHerreroo@users.noreply.github.com>
2025-01-20 22:36:11 +0100

    Clases Mejoradas

--- UNO_TCP/src/Carta.java
+++ UNO_TCP/src/Carta.java
@@ -1,14 +1,28 @@
-import java.util.Collection;
+import java.util.Random;
 
 public class Carta {
-    private int numero;
 
-    private enum colores {
+    private int numero;
+    private String color;
+
+    private enum coloresEnum {
         ROJO,
         AMARILLO,
         VERDE,
         AZUL
     }
 
+    public int getRandomNum() {
+        return new Random().nextInt(10);
+    }
 
+    public String getRandomColor() {
+        int randIndex = new Random().nextInt(coloresEnum.values().length);
+        return coloresEnum.values()[randIndex].toString();
+    }
+
+    public static void main(String[] args) {
+        Carta c = new Carta();
+        System.out.println(c.getRandomColor() + " " + c.getRandomNum());
+    }
 }
--- UNO_TCP/src/Jugador.java
+++ UNO_TCP/src/Jugador.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Jugador {
 
     private String nombre;
+
+    private Map<Integer, String> mano = new HashMap<Integer,String>();
+
+
+    public void agregarCarta(int num, String color) {
+        mano.put(num,color);
+    }
+
+    public static void main(String[] args) {
+        Jugador j = new Jugador();
+        Carta c = new Carta();
+
+        j.mano.put(c.getRandomNum(),c.getRandomColor());
+        j.mano.put(c.getRandomNum(),c.getRandomColor());
+        j.mano.put(c.getRandomNum(),c.getRandomColor());
+        j.mano.put(c.getRandomNum(),c.getRandomColor());
+        j.mano.put(c.getRandomNum(),c.getRandomColor());
+        j.mano.put(c.getRandomNum(),c.getRandomColor());
+        j.mano.put(c.getRandomNum(),c.getRandomColor());
+        j.mano.put(c.getRandomNum(),c.getRandomColor());
+
+        j.mano.forEach((num, color) -> System.out.println("Num: " + num + ", Color: " + color));
+    }
 
 }
